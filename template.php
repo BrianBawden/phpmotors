56477d2e0b187ba212e54113049fce033b33f1f1
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 18:38:51 -0400

    work on my account location.

--- template.php
+++ template.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/base.css" media="screen" />
     <link rel="stylesheet" href="css/large.css" media="screen" />
+    <link rel="stylesheet" href="css/small.css" media="screen" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <!-- link for google font  Cairo-->
     <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
