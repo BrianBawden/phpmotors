36359109dab483352a44d2991a130033ca47e5fb
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 11:36:23 -0400

    format edits.

--- template.php
+++ template.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/base.css" media="screen"/>
-    <link rel="stylesheet" href="css/large.css" media="screen"/>
+    <link rel="stylesheet" href="css/base.css" media="screen" />
+    <link rel="stylesheet" href="css/large.css" media="screen" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <!-- link for google font  Cairo-->
     <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
@@ -19,17 +19,17 @@
         <div>
             <a href="#"><img src="images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
             <a class="plain_link" id="log_in" href="#">My Account</a>
-            </nav>
-            <ul>
-                <li><a class="plain_link" href="#" onclick="toggleMenu()">&#9776;</a></li>
-                <li><a class="plain_link" href="#">Home</a></li>
-                <li><a class="plain_link" href="#">Classic</a></li>
-                <li><a class="plain_link" href="#">Sports</a></li>
-                <li><a class="plain_link" href="#">SUV</a></li>
-                <li><a class="plain_link" href="#">Trucks</a></li>
-                <li><a class="plain_link" href="#">Used</a></li>
-            </ul>
             <nav>
+                <ul>
+                    <li><a class="plain_link" href="#" onclick="toggleMenu()">&#9776;</a></li>
+                    <li><a class="plain_link" href="#">Home</a></li>
+                    <li><a class="plain_link" href="#">Classic</a></li>
+                    <li><a class="plain_link" href="#">Sports</a></li>
+                    <li><a class="plain_link" href="#">SUV</a></li>
+                    <li><a class="plain_link" href="#">Trucks</a></li>
+                    <li><a class="plain_link" href="#">Used</a></li>
+                </ul>
+            </nav>
         </div>
     </header>
     <main>
