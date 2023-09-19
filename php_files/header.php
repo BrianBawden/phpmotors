cf740a912d437bda4bd98cbd75d9e9e5722f496b
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 11:40:58 -0400

    create header.php with header html.

---
+++ php_files/header.php
@@ -0,0 +1,15 @@
+<div>
+            <a href="#"><img src="images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
+            <a class="plain_link" id="log_in" href="#">My Account</a>
+            <nav>
+                <ul>
+                    <li><a class="plain_link" href="#" onclick="toggleMenu()">&#9776;</a></li>
+                    <li><a class="plain_link" href="#">Home</a></li>
+                    <li><a class="plain_link" href="#">Classic</a></li>
+                    <li><a class="plain_link" href="#">Sports</a></li>
+                    <li><a class="plain_link" href="#">SUV</a></li>
+                    <li><a class="plain_link" href="#">Trucks</a></li>
+                    <li><a class="plain_link" href="#">Used</a></li>
+                </ul>
+            </nav>
+        </div>
