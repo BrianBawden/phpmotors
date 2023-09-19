655485d364055e6093291a5f4276da7bcca2d2ee
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 10:06:22 -0400

    add font comic neue and cairo.

--- template.php
+++ template.php
@@ -5,6 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/base.css" media="screen"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- links for google font Comic Neue -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
+    <!-- link for google font  Cairo-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500&display=swap" rel="stylesheet">
     <title>Template</title>
 </head>
 
