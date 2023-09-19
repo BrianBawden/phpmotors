09269dbbe716baa219ffa4dac31d4b2f4f4d5777
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 09:57:42 -0400

    uncomment hamburger and add id for login

--- template.php
+++ template.php
@@ -12,7 +12,7 @@
     <header>
         <div>
             <a href="#"><img src="images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
-            <a class="plain_link" href="#">My Account</a>
+            <a class="plain_link" id="log_in" href="#">My Account</a>
             </nav>
             <ul>
                 <li><a class="plain_link" href="#" onclick="toggleMenu()">&#9776;</a></li>
@@ -30,6 +30,7 @@
         <h1>Content Title Here</h1>
     </main>
     <footer>
+        <hr>
         <p>PHP Motors All Rights Reserved.</p>
         <p>All images used are believed to be in "Fair Use." Please notify author if any are not and they will be removed.</p>
         <p>Last updated September 19, 2023</p>
