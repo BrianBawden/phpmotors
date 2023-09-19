ecce08790b1a231b3dff3c74ef7b8e9e2ff279eb
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 11:46:25 -0400

    switch header to php code.

--- template.php
+++ template.php
@@ -16,21 +16,7 @@
 
 <body>
     <header>
-        <div>
-            <a href="#"><img src="images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
-            <a class="plain_link" id="log_in" href="#">My Account</a>
-            <nav>
-                <ul>
-                    <li><a class="plain_link" href="#" onclick="toggleMenu()">&#9776;</a></li>
-                    <li><a class="plain_link" href="#">Home</a></li>
-                    <li><a class="plain_link" href="#">Classic</a></li>
-                    <li><a class="plain_link" href="#">Sports</a></li>
-                    <li><a class="plain_link" href="#">SUV</a></li>
-                    <li><a class="plain_link" href="#">Trucks</a></li>
-                    <li><a class="plain_link" href="#">Used</a></li>
-                </ul>
-            </nav>
-        </div>
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/header.php'; ?>
     </header>
     <main>
         <h1>Content Title Here</h1>
