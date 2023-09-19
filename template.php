7e56b4a2893578a682397230e06f835ca6a69c4e
Brian Bawden <brianbawden001@gmail.com>
2023-09-19 08:02:08 -0400

    Init template.php page.

---
+++ template.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <a href="#"><img src="images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
+            <a href="#">My Account</a>
+            </nav>
+            <ul>
+                <li>Home</li>
+                <li>Classic</li>
+                <li>Sports</li>
+                <li>SUV</li>
+                <li>Trucks</li>
+                <li>Used</li>
+            </ul>
+            <nav>
+        </div>
+    </header>
+    <main>
+        <h1>Content Title Here</h1>
+    </main>
+    <footer>
+        <p>PHP Motors All Rights Reserved.</p>
+        <p>All images used are believed to be in "Fair Use." Please notify author if any are not and they will be removed.</p>
+        <p>Last updated September 19, 2023</p>
+    </footer>
+
+</body>
+
+</html>
