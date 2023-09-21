f51192bf55df3103489acf57a3649a56b655d734
Brian Bawden <brianbawden001@gmail.com>
2023-09-21 10:31:45 -0400

    change head to use php

--- home.php
+++ home.php
@@ -2,17 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/base.css" media="screen" />
-    <link rel="stylesheet" href="css/large.css" media="screen" />
-    <!-- <link rel="stylesheet" href="css/small.css" media="screen" /> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <!-- link for google font  Cairo-->
-    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
-    <!-- links for google font Comic Neue -->
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500&display=swap" rel="stylesheet">
-    <title>PHP Motors</title>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/head.php'; ?>
 </head>
 
 <body>
@@ -83,8 +73,6 @@
                 </div>
             </div>
         </div>
-
-
     </main>
     <footer>
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/footer.php'; ?>
--- template.php
+++ template.php
@@ -2,17 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/base.css" media="screen" />
-    <link rel="stylesheet" href="css/large.css" media="screen" />
-    <!-- <link rel="stylesheet" href="css/small.css" media="screen" /> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <!-- link for google font  Cairo-->
-    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
-    <!-- links for google font Comic Neue -->
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@500&display=swap" rel="stylesheet">
-    <title>Template</title>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/head.php'; ?>
 </head>
 
 <body>
@@ -23,7 +13,7 @@
         <h1>Content Title Here</h1>
     </main>
     <footer>
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/footer.php';?>
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/backend1/phpmotors/php_files/footer.php'; ?>
     </footer>
 
 </body>
