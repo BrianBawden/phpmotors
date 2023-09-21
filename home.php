e06e781fe1baee8e3e22107b6f43ebbeb1e35f5d
Brian Bawden <brianbawden001@gmail.com>
2023-09-21 12:18:01 -0400

    set .upgrade and .reviews side by side with large view.

--- home.php
+++ home.php
@@ -33,43 +33,45 @@
             <img src="images/delorean.jpg" alt="Cartoon of Delorean">
             <button>Own Today</button>
         </div>
-        <div class="reviews">
-            <h2>DMC Delorean Reviews</h2>
-            <ul>
-                <li>"So fast it is almost like traveling in time." (4/5)</li>
-                <li>"Coolest ride on the road." (4/5)</li>
-                <li>"I'm feeling Marty McFly!"(5/5)</li>
-                <li>"The most futuristic ride of our day."(4.5/5)</li>
-                <li>"80's livin and I love it!"(5/5)</li>
+        <div class="rAndU">
+            <div class="reviews">
+                <h2>DMC Delorean Reviews</h2>
+                <ul>
+                    <li>"So fast it is almost like traveling in time." (4/5)</li>
+                    <li>"Coolest ride on the road." (4/5)</li>
+                    <li>"I'm feeling Marty McFly!"(5/5)</li>
+                    <li>"The most futuristic ride of our day."(4.5/5)</li>
+                    <li>"80's livin and I love it!"(5/5)</li>
 
-            </ul>
-        </div>
-        <div class="upgrade">
-            <h2>Delorean Upgrades</h2>
-            <div class="upgrade_table">
-                <div class="upgrade_options">
-                    <figure>
-                        <img src="images/upgrades/flux-cap.png" alt="flux_capacitor">
-                        <figcaption>Flux Capacitor</figcaption>
-                    </figure>
-                </div>
-                <div class="upgrade_options">
-                    <figure>
-                        <img src="images/upgrades/flame.jpg" alt="Flame Decal">
-                        <figcaption>Flame decal</figcaption>
-                    </figure>
-                </div>
-                <div class="upgrade_options">
-                    <figure>
-                        <img src="images/upgrades/bumper_sticker.jpg" alt="bumper sticker">
-                        <figcaption>Bumper sticker</figcaption>
-                    </figure>
-                </div>
-                <div class="upgrade_options">
-                    <figure>
-                        <img src="images/upgrades/hub-cap.jpg" alt="Hubcap">
-                        <figcaption>Hubcap</figcaption>
-                    </figure>
+                </ul>
+            </div>
+            <div class="upgrade">
+                <h2>Delorean Upgrades</h2>
+                <div class="upgrade_table">
+                    <div class="upgrade_options">
+                        <figure>
+                            <img src="images/upgrades/flux-cap.png" alt="flux_capacitor">
+                            <figcaption>Flux Capacitor</figcaption>
+                        </figure>
+                    </div>
+                    <div class="upgrade_options">
+                        <figure>
+                            <img src="images/upgrades/flame.jpg" alt="Flame Decal">
+                            <figcaption>Flame decal</figcaption>
+                        </figure>
+                    </div>
+                    <div class="upgrade_options">
+                        <figure>
+                            <img src="images/upgrades/bumper_sticker.jpg" alt="bumper sticker">
+                            <figcaption>Bumper sticker</figcaption>
+                        </figure>
+                    </div>
+                    <div class="upgrade_options">
+                        <figure>
+                            <img src="images/upgrades/hub-cap.jpg" alt="Hubcap">
+                            <figcaption>Hubcap</figcaption>
+                        </figure>
+                    </div>
                 </div>
             </div>
         </div>
