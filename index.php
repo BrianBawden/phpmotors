e3bafb28e6248c7640312bdda5e4f13040b772fb
Brian Bawden <brianbawden001@gmail.com>
2023-10-04 14:59:26 -0400

    change header for controller code.

--- index.php
+++ index.php
@@ -12,9 +12,7 @@ $action = filter_input(INPUT_POST, 'action');
 require_once 'library/connections.php';
 // Get the PHP Motors model for use as needed
 require_once 'model/main-model.php';// Get the database connection file
-require_once 'library/connections.php';
-// Get the PHP Motors model for use as needed
-require_once 'model/main-model.php';
+
 
 // Get the array of classifications
 $classifications = getClassifications();
@@ -39,6 +37,7 @@ $navList .= '</ul>';
 
 // switch statement reading the $action value to know what view to show, with a default of view/home.php
  switch ($action){
+
  case 'template':
     include 'view/template.php';
   
