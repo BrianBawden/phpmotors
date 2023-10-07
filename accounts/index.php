99b6f71f8ca62f2b7accec639b43d604b256e0fa
Brian Bawden <brianbawden001@gmail.com>
2023-10-06 21:40:17 -0400

    init accounts index.php.

---
+++ accounts/index.php
@@ -0,0 +1,51 @@
+<?php
+// This is the accounts controller for phpmotors website.
+
+// set $action to the filter input post/get for security reasons.
+$action = filter_input(INPUT_POST, 'action');
+ if ($action == NULL){
+  $action = filter_input(INPUT_GET, 'action');
+ }
+
+
+// Get the database connection file
+require_once '../library/connections.php';
+// Get the PHP Motors model for use as needed
+require_once '../model/main-model.php';// Get the database connection file
+
+
+// Get the array of classifications from the database.
+$classifications = getClassifications();
+
+// The commented out code below is to test the connection to the database.
+
+// var_dump($classifications);
+// 	exit;
+
+// Build a navigation bar using the $classifications array
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach ($classifications as $classification) {
+ $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
+// The commented out code below is to test the nav bar code. 
+
+// echo $navList;
+// exit;
+
+// switch statement reading the $action value to know what view to show, with a default of view/home.php
+ switch ($action){
+
+ case '':
+  
+  break;
+ 
+ default:
+
+ break;
+}
+
+
+?>
