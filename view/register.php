8a8ff64870010b760005a075d5a5680845518011
Brian Bawden <brianbawden001@gmail.com>
2023-10-10 10:46:21 -0400

    add echo message, change form; method/action.

--- view/register.php
+++ view/register.php
@@ -17,7 +17,13 @@
     </header>
     <main>
         <h1>Register</h1>
-        <form>
+
+        <?php
+            if (isset($message)) {
+            echo $message;
+            }
+        ?>
+        <form method="post" action="/phpmotors/accounts/index.php">
             <label for="fName"><span class="required">*</span><strong>First Name:</strong></label><br>
             <input type="text" id="fName" name="fName" required><br>
 
@@ -30,9 +36,12 @@
             <label for="password"><span class="required">*</span><strong>Password:</strong></label><br>
             <input type="password" id="password" name="password" required><br>
             <p>Password must be at least 8 characters and include: one capital case, one lower case, and one number.</p>
-            <button>Register</button>
+            <input type="submit" name="submit" id="regbtn" value="Register">
+            <!-- Add the action name - value pair -->
+            <input type="hidden" name="action" value="register">
         </form>
         <p>Not a member? <a class="plain_link" href="#">Sign up</a></p>
+
     </main>
     <footer>
         <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/php_files/footer.php'; ?>
