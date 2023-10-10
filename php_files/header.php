9c5d1e2fbc9d173555bf3ff23e5b0d8af3749ad3
Brian Bawden <brianbawden001@gmail.com>
2023-10-10 12:39:17 -0400

    my account link pointing to accounts index.php

--- php_files/header.php
+++ php_files/header.php
@@ -1,4 +1,4 @@
 <div>
     <a id="logo" href="#"><img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo"></a>
-    <a class="plain_link" id="log_in" href="/phpmotors/index.php?action=sign_in">My Account</a>
+    <a class="plain_link" id="log_in" href="/phpmotors/accounts/index.php?action=sign_in">My Account</a>
 </div>
