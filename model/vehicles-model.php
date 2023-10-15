1a283d8302b7e123262af7de1a1c5252ce0281b2
Brian Bawden <brianbawden001@gmail.com>
2023-10-15 19:38:48 -0400

    create insertNewVehicle function.

--- model/vehicles-model.php
+++ model/vehicles-model.php
@@ -1,6 +1,7 @@
 <?php
 // Vehicles model
 
+
 function insertNewClassification($classificationName){
     // object connected to phpmotors database using connection function.
     $db = phpmotorsConnect();
@@ -13,6 +14,74 @@ function insertNewClassification($classificationName){
 
     //replace placeholders with sql values with type and data.
     $stmt->bindValue(':classificationName', $classificationName, PDO::PARAM_STR);
+
+    // execute INSERT $stmt
+    $stmt->execute();
+
+    // Check if success
+    $rowsChanged = $stmt->rowCount();
+
+    // close db
+    $stmt->closeCursor();
+
+    return $rowsChanged;
+}
+
+
+// Add new vehicle to phpmotors database. primary key auto assigned. 
+function insertNewVehicle(
+    $invMake,
+    $invModel,
+    $invDescription,
+    $invImage,
+    $invThumbnail,
+    $invPrice,
+    $invStock,
+    $invColor,
+    $classificationId
+    ){
+
+    // object connected to phpmotors database using connection function.
+    $db = phpmotorsConnect();
+
+    // SQL statement to insert new inventory.
+    $sql = 'INSERT INTO inventory 
+    (
+    invMake,
+    invModel,
+    invDescription,
+    invImage,
+    invThumbnail,
+    invPrice,
+    invStock,
+    invColor,
+    classificationId
+    )
+    
+    VALUES (
+    :invMake,
+    :invModel,
+    :invDescription,
+    :invImage,
+    :invThumbnail,
+    :invPrice,
+    :invStock,
+    :invColor,
+    :classificationId)';
+
+    // prepare statement 
+    $stmt = $db->prepare($sql);
+
+    //replace placeholders with sql values with type and data.
+    $stmt->bindValue(':invMake',          $invMake,          PDO::PARAM_STR);
+    $stmt->bindValue(':invModel',         $invModel,         PDO::PARAM_STR);
+    $stmt->bindValue(':invDescription',   $invDescription,   PDO::PARAM_STR);
+    $stmt->bindValue(':invImage',         $invImage,         PDO::PARAM_STR);
+    $stmt->bindValue(':invThumbnail',     $invThumbnail,     PDO::PARAM_STR);
+    $stmt->bindValue(':invPrice',         $invPrice,         PDO::PARAM_STR);
+    $stmt->bindValue(':invStock',         $invStock,         PDO::PARAM_STR);
+    $stmt->bindValue(':invColor',         $invColor,         PDO::PARAM_STR);
+    $stmt->bindValue(':classificationId', $classificationId, PDO::PARAM_STR);
 
     // execute INSERT $stmt
     $stmt->execute();
