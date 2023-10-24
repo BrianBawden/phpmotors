9a6d0db8a95286ccd01a0046400feb4548491585
Brian Bawden <brianbawden001@gmail.com>
2023-10-23 21:32:57 -0400

    vehicles index.php add filters/sanitizer for newVehicle and class

--- vehicles/index.php
+++ vehicles/index.php
@@ -9,6 +9,8 @@ if ($action == NULL){
 
 // connect to database
 require_once '../library/connections.php';
+// connect to functions.php library
+require_once '../library/functions.php';
 // connect to model/main-model.php
 require_once '../model/main-model.php';
 // connect to model/vehicles-model.php
@@ -65,11 +67,11 @@ switch($action){
   case 'newClass':
     
     // filter and store data
-    $classificationName = filter_input(INPUT_POST, 'classificationName');
+    $classificationName = trim(filter_input(INPUT_POST, 'classificationName', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
 
     // check for missing data
     if(empty($classificationName)){
-      $message = '<p id="errorMsg">Please provide information for all empty form fields.</p>';
+      $message = '<p id="errorMsg">Error adding Classification, please try again.</p>';
       include '../view/add-classification.php';
       exit;
     }
@@ -92,15 +94,15 @@ switch($action){
 
       
       // filter and store data
-      $invMake          = filter_input(INPUT_POST, 'invMake');
-      $invModel         = filter_input(INPUT_POST, 'invModel');
-      $invDescription   = filter_input(INPUT_POST, 'invDescription');
-      $invImage         = filter_input(INPUT_POST, 'invImage');
-      $invThumbnail     = filter_input(INPUT_POST, 'invThumbnail');
-      $invPrice         = filter_input(INPUT_POST, 'invPrice');
-      $invStock         = filter_input(INPUT_POST, 'invStock');
-      $invColor         = filter_input(INPUT_POST, 'invColor');
-      $classificationId = filter_input(INPUT_POST, 'classificationId');
+      $invMake          = trim(filter_input(INPUT_POST, 'invMake', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $invModel         = trim(filter_input(INPUT_POST, 'invModel', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $invDescription   = trim(filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $invImage         = trim(filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $invThumbnail     = trim(filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $invPrice         = trim(filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION));
+      $invStock         = trim(filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_ALLOW_FRACTION));
+      $invColor         = trim(filter_input(INPUT_POST, 'invColor', FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+      $classificationId = trim(filter_input(INPUT_POST, 'classificationId', FILTER_SANITIZE_NUMBER_INT));
       
       
       // check for missing data
