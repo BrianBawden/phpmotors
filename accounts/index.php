e94175b974a5c01c09b6ee921da58a0841cc7a89
Brian Bawden <brianbawden001@gmail.com>
2023-11-01 08:48:09 -0400

    logic to verify username/password

--- accounts/index.php
+++ accounts/index.php
@@ -57,6 +57,35 @@ switch ($action){
       include '../view/login.php';
       exit; 
     }
+
+    // A valid password exists, proceed with the login process
+    // Query the client data based on the email address
+    $clientData = getClient($clientEmail); // array of client info that matches email.
+
+    // Compare the password just submitted against
+    // the hashed password for the matching client
+    $hashCheck = password_verify($clientPassword, $clientData['clientPassword']); // true/false if parameters match.
+
+    // If the hashes don't match create an error
+    // and return to the login view
+    if(!$hashCheck) {
+      $message = '<p id="errorMsg">Please check your password and try again.</p>';
+      include '../view/login.php';
+      exit;
+    }
+
+    // A valid user exists, log them in
+    $_SESSION['loggedin'] = TRUE;
+
+    // Remove the password from the array
+    // the array_pop function removes the last element from an array
+    array_pop($clientData);
+
+    // Store the array into the session
+    $_SESSION['clientData'] = $clientData;
+    // Send them to the admin view
+    include '../view/admin.php';
+    exit;
     break;
     
     case 'register':
