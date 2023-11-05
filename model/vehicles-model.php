06f99ce00d97795f1ce199d89ec93aa7ac532217
Brian Bawden <brianbawden001@gmail.com>
2023-11-05 11:39:37 -0500

    week9 select vehicles done.

--- model/vehicles-model.php
+++ model/vehicles-model.php
@@ -98,4 +98,16 @@ function insertNewVehicle(
     return $rowsChanged;
 }
 
+// Get vehicles by classificationId 
+function getInventoryByClassification($classificationId){ 
+    $db = phpmotorsConnect(); 
+    $sql = ' SELECT * FROM inventory WHERE classificationId = :classificationId'; 
+    $stmt = $db->prepare($sql); 
+    $stmt->bindValue(':classificationId', $classificationId, PDO::PARAM_INT); 
+    $stmt->execute(); 
+    $inventory = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    $stmt->closeCursor(); 
+    return $inventory; 
+   }
+
 ?>
